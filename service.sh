9fa5371af4871a4b97a8a8e8cbd21cf8bd677edc
Axel Svensson <mail@axelsvensson.com>
2021-03-27 20:12:11 +0100

    Install node at build time

--- service.sh
+++ service.sh
@@ -4,12 +4,9 @@ finish() { if [ $? -gt 0 ]; then echo FAILED; exit 1; fi; }
 trap finish EXIT
 
 export NVMDIR="$PWD/.nvm"
-[ -d "$NVMDIR" ] || git clone https://github.com/nvm-sh/nvm.git "$NVMDIR"
-( cd "$NVMDIR"; git pull )
 set +e
 [ -s "$NVMDIR/nvm.sh" ] && \. "$NVMDIR/nvm.sh"
 set -e
-nvm install
 nvm use
 
 mkdir -p /var/local/score-voting
